df14f11bab67a80076a4e298ec5003f74cd64e90
runnytu <runnytu@gmail.com>
2018-02-06 23:27:11 +0100

    Fix missing hash in osdep_service.h

--- os_dep/linux/mlme_linux.c
+++ os_dep/linux/mlme_linux.c
@@ -113,7 +113,7 @@ static void _dynamic_check_timer_handlder(void *FunctionContext)
 static void _dynamic_check_timer_handlder(struct timer_list *t)
 #endif
 {
-if LINUX_VERSION_CODE < KERNEL_VERSION(4, 15, 0)
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 15, 0)
         _adapter *adapter = (_adapter *)FunctionContext;
 #else
         _adapter *adapter = from_timer(adapter, t, mlmepriv.dynamic_chk_timer);
