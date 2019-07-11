2f910b90d8027ca2ee3809ffcde642a965d8f52f
runnytu <runnytu@gmail.com>
2019-07-11 21:04:57 +0200

    Update support kernel 5.2

--- os_dep/linux/os_intfs.c
+++ os_dep/linux/os_intfs.c
@@ -958,15 +958,13 @@ unsigned int rtw_classify8021d(struct sk_buff *skb)
 	return dscp >> 5;
 }
 
-static u16 rtw_select_queue(struct net_device *dev, struct sk_buff *skb	
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 19, 0)
-				, struct net_device *sb_dev
-#elif LINUX_VERSION_CODE >= KERNEL_VERSION(3, 13, 0)
-				, void *accel_priv
-#endif
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(3, 14, 0) 
-				, select_queue_fallback_t fallback
-#endif
+#if (LINUX_VERSION_CODE>=KERNEL_VERSION(5,2,0))
+static u16 rtw_select_queue(struct net_device *dev, struct sk_buff *skb,
+			    struct net_device *sb_dev)
+#elif (LINUX_VERSION_CODE>=KERNEL_VERSION(4,19,0))
+static u16 rtw_select_queue(struct net_device *dev, struct sk_buff *skb,
+ 			    struct net_device *sb_dev,
+ 			    select_queue_fallback_t fallback)
 )
 {
 	_adapter	*padapter = rtw_netdev_priv(dev);
